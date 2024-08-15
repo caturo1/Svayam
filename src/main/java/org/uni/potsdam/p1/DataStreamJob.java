4d1a1b5d41e0b70417dd319c612e6f8da9414dff
olaf-link <link1@uni-potsdam.de>
2024-08-15 19:54:54 +0200

    Add logic to create a list of sources and sinks as parameters for an execution job
    Add a separate execute method to run the main stream processing job

--- src/main/java/org/uni/potsdam/p1/DataStreamJob.java
+++ src/main/java/org/uni/potsdam/p1/DataStreamJob.java
@@ -18,28 +18,52 @@
 
 package org.uni.potsdam.p1;
 
+import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.connector.sink2.Sink;
 import org.apache.flink.connector.datagen.source.DataGeneratorSource;
 import org.apache.flink.connector.datagen.source.GeneratorFunction;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.PrintSink;
 import org.uni.potsdam.p1.types.Measurement;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataStreamJob {
 
-  public static int BATCH_SIZE = 10000;
-  public static GeneratorFunction<Long, Measurement>
+  // define the number of sinks and sources
+  private static final int NUMBER_OF_SINKS = 2;
+  private static final int NUMBER_OF_SOURCES = 2;
+
+  // define the number of entries and
+  private static final int BATCH_SIZE = 10000;
+  private static GeneratorFunction<Long, Measurement>
     MEASUREMENT_GENERATOR = index -> new Measurement();
 
-  public static List<DataGeneratorSource<?>> sources;
-  public static List<Sink> sinks;
+  private final List<DataGeneratorSource<Measurement>> sources = new ArrayList<>();
+  private final List<Sink<Measurement>> sinks = new ArrayList<>();
 
-  public static void main(String[] args) throws Exception {
-    final StreamExecutionEnvironment env =
-      StreamExecutionEnvironment.getExecutionEnvironment();
-
-    env.execute("Flink Java API Skeleton");
+  public DataStreamJob(int numberOfSources, int numberOfSinks) {
+    for (int i = 0; i < numberOfSources; i++) {
+      sources.add(new DataGeneratorSource<>(
+        MEASUREMENT_GENERATOR,
+        BATCH_SIZE,
+        TypeInformation.of(Measurement.class)));
+    }
+    for (int i = 0; i < numberOfSinks; i++) {
+      sinks.add(new PrintSink<>());
+    }
   }
 
+  public static void main(String[] args) throws Exception {
+    DataStreamJob job = new DataStreamJob(NUMBER_OF_SOURCES, NUMBER_OF_SINKS);
+    job.execute();
+  }
+
+  public JobExecutionResult execute() throws Exception {
+    final StreamExecutionEnvironment env =
+      StreamExecutionEnvironment.getExecutionEnvironment();
+    return env.execute("Flink Java API Skeleton");
+  }
 }
